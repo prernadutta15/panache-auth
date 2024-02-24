20432f1ee5f0a0d2028933272f097dd4e1ee38e3
prernadutta15 <prerna46_scs@jnu.ac.in>
2024-02-24 21:59:54 +0530

    Added custom claims in token

--- src/main/java/org/example/panacheauth/security/models/CustomGrantedAuthority.java
+++ src/main/java/org/example/panacheauth/security/models/CustomGrantedAuthority.java
@@ -1,9 +1,10 @@
 package org.example.panacheauth.security.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.NoArgsConstructor;
 import org.example.panacheauth.models.Role;
 import org.springframework.security.core.GrantedAuthority;
-@NoArgsConstructor
+@NoArgsConstructor @JsonDeserialize
 public class CustomGrantedAuthority implements GrantedAuthority {
 //    private Role role;
     private String authority;
